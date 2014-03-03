82d3abdb0daa708e8d6064db8d8b5d2d558a84e8
Joel Frederico <joelfred@slac.stanford.edu>
2014-03-03 01:49:04 -0800

    Updated analysis to output PDF of all of the shots.

--- analyze_matlab.py
+++ analyze_matlab.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python
+
+# System imports
 import argparse
-import numpy as np
-import scipy.io as sio
-import ButterflyEmittancePython as bt
-import mytools.slactrac as sltr
-import matplotlib.pyplot as plt
-import mytools as mt
-import mytools.E200 as E200
 import copy
 import h5py as h5
-import pdb as _pdb
+import pdb
+
+# Math/science imports
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.gridspec as gs
+
+# My module imports
+import ButterflyEmittancePython as bt
+import mytools.slactrac as sltr
+import mytools as mt
+import mytools.E200 as E200
+
+# Allow for standalone iPython debugging.
+# import sys
+# from IPython.core import ultratb
+# sys.excepthook = ultratb.FormattedTB(mode='Verbose',
+# color_scheme='Linux', call_pdb=1)
 
 plt.close('all')
 def wrap_analyze(infile):
+	# ======================================
+	# Start PDF creation
+	# ======================================
+	# Create PDF
+	pp = PdfPages('output.pdf')
+	# Create figure
+	fig=mt.figure('Page 1',figsize=(8.5,11))
+	# Create larger gridspec
+	outergs= gs.GridSpec(5,1)
+
 	# ======================================
 	# Load and transfer matlab variables
 	# ======================================
@@ -20,49 +44,103 @@ def wrap_analyze(infile):
 	data=f['data']
 	
 	# ======================================
-	# Load an image
+	# Get step info and setup
 	# ======================================
 	step_value=data['raw']['scalars']['step_value']
-	zero_uids=mt.E200.E200_api_getUID(step_value,0,f)
+	step_vals=np.unique(E200.E200_api_getdat(step_value,f))
+	# Artificially limit step_vals to =0GeV
+	step_vals=np.array([0])
+	numvals = step_vals.shape[0]
+	emit_array=np.zeros((numvals,50))
+
+	# for i,stepval in enumerate(step_vals[2:]):
+	# ======================================
+	# Run over steps
+	# ======================================
+	for i,stepval in enumerate(step_vals):
+		print 'We\'re on step {} with value {}.'.format(i,stepval)
+
+		zero_uids=mt.E200.E200_api_getUID(step_value,stepval,f)
+		print zero_uids
+		cegain = data['raw']['images']['CEGAIN']
+		img_uids=cegain['UID'][:,0]
+		# wantedUIDs=zero_uids[16]
+		valid_img_uids=np.intersect1d(zero_uids,img_uids)
+		for j,stepuid in enumerate(valid_img_uids):
+			try:
+				# ======================================
+				# Save all figs to one page
+				# ======================================
+				# Use correct line
+				linnum=np.mod(j,5)
+				if linnum == 0:
+					if j>0:
+						plt.tight_layout()
+						pp.savefig(figure=fig)
+					plt.close('all')
+					fig=mt.figure('Steps {}-{}'.format(j+1,j+5),figsize=(8.5,11))
+				innergs=gs.GridSpecFromSubplotSpec(1,2,subplot_spec=outergs[linnum])
+				aximg=plt.Subplot(fig,innergs[0,0])
+				axplotfit=plt.Subplot(fig,innergs[0,1])
+				# pdb.set_trace()
+				# pp.savefig()
+				out = single_analysis(
+						data,
+						stepuid,
+						f,
+						aximg=aximg,
+						axplotfit=axplotfit
+						)
+				fig.add_subplot(aximg)
+				fig.add_subplot(axplotfit)
+				emit_array[i,j]=out.emit
+				# plt.show()
+				# plt.clear('all')
+				# pdb.set_trace()
+				
+			except KeyboardInterrupt:
+				raise
+			except:
+				# raise
+				pass
+
+
+		if i > 0:
+			pdb.set_trace()
+
+	# pdb.set_trace()
+	pp.close()
+	return emit_array
+
+
+def single_analysis(data,wantedUIDs,f,plot=False,aximg=None,axplotfit=None):
+	# ======================================
+	# Set up plots and PDF saving
+	# ======================================
+	if plot:
+		plt.close('all')
+
+	# ======================================
+	# Load the right image
+	# ======================================
 	cegain = data['raw']['images']['CEGAIN']
 	img_uids=cegain['UID'][:,0]
-	# wantedUIDs=zero_uids[16]
-	valid_img_uids=np.intersect1d(zero_uids,img_uids)
-	wantedUIDs=valid_img_uids[17]
 	wantedUIDs_bool=np.in1d(img_uids,wantedUIDs)
 	imgs=cegain['dat'][:,0]
 	img=imgs[wantedUIDs_bool]
 	img=mt.E200.E200_load_images(img,f)
 	img=img[0]
 	# Show image loaded
-	# mt.figure('Image in')
-	# plt.imshow(img)
+	if plot:
+		plt.imshow(img)
+		if plot:
+			plt.show()
 
-	
-	# ======================================
-	# Load Matlab-processed info
-	# ======================================
-	processed_data = f['processed_data']
-	img_sub        = f['img_sub']
-	hist_data      = f['hist_data']
-	sum_x     = np.array(processed_data['sum_x'])
-	sum_y     = np.array(processed_data['sum_y'])
-	x_meter   = np.array(processed_data['x_meter'])
-
-	# ======================================
-	# Load quad values
-	# ======================================
-	step_uids=step_value['UID'][:,0]
-	step_vals=E200.E200_api_getdat(step_value,wantedUIDs,f)
-	bact = E200.setQS.set_QS_energy_ELANEX(step_vals[0])
-	qs1_k_half = E200.setQS.bdes2K1(bact[0],20.35)
-	qs2_k_half = E200.setQS.bdes2K1(bact[1],20.35)
-
-	# ======================================
-	# Load mag energy/gamma
-	# ======================================
-	B_spect = f[f['data']['raw']['scalars']['LI20_LGPS_3330_BDES']['dat'][0,0]][0,0]
-	gamma     = (20.35/0.5109989)*1e3
+	if aximg is not None:
+		aximg.imshow(img)
+		mt.figure('this')
+		plt.imshow(img)
+		# plt.show()
 
 	# ======================================
 	# Set up image slices
@@ -71,10 +149,18 @@ def wrap_analyze(infile):
 	res_x    = res_y / np.sqrt(2)
 
 	# ======================================
+	# Load quad values
+	# ======================================
+	step_value=data['raw']['scalars']['step_value']
+	# step_uids=step_value['UID'][:,0]
+	step_vals=E200.E200_api_getdat(step_value,f,wantedUIDs)
+	E_offset=step_vals[0]
+
+	# ======================================
 	# Image slice setup
 	#	-Use ~2% bandwidth
 	# ======================================
-	E0    = 20.35
+	E0    = 20.35 + E_offset
 	# bandE = 0.01*E0
 	bandE = 0.008*E0
 	Emin  = E0-bandE
@@ -92,10 +178,29 @@ def wrap_analyze(infile):
 	# Right side doesn't have this issue.
 	x_min = 200
 	x_max = xsize
+	# More realistic bounds
 	x_min = 500
 	x_max = 750
+
+	# ======================================
+	# Plot image
+	# ======================================
+	if plot:
+		fig=mt.figure('Roi\'d image')
+		plt.imshow(img[y_min:y_max,x_min:x_max])
+		pdb.set_trace()
+		if plot:
+			plt.show()
 	
-	mt.findpinch(img,xbounds=(x_min,x_max),ybounds=(y_min,y_max),step=1)
+	# ======================================
+	# Find the pinch
+	# ======================================
+	# mt.findpinch(
+	#                 img,
+	#                 xbounds=(x_min,x_max),
+	#                 ybounds=(y_min,y_max),
+	#                 step=2,
+	#                 verbose=True)
 
 	# ======================================
 	# Define strip edges.
@@ -123,19 +228,37 @@ def wrap_analyze(infile):
 		# Convert to y values
 		ylow  = bt.E_to_y(Elow,f,res_y)
 		yhigh = bt.E_to_y(Ehigh,f,res_y)
-		# gaussout = mt.fitimageslice(img,res/1e6,(x_min,x_max),(ylow,yhigh))
-		eavg[i], gaussout = mt.fitimageslice(img,res_x,res_y,(x_min,x_max),(ylow,yhigh),avg_e_func=bt.avg_E,h5file=f)
+		eavg[i], gaussout = mt.fitimageslice(
+				img,
+				res_x,
+				res_y,
+				(x_min,x_max),
+				(ylow,yhigh),
+				avg_e_func=bt.avg_E,
+				h5file=f,
+				plot=False)
+
 		chisq_red[i] = gaussout.chisq_red
 		variance[i]         = gaussout.popt[2]
-		varerr[i]           = gaussout.pcov[2,2]
-		stddev[i]           = np.sqrt(gaussout.pcov[2,2])
+		try:
+			varerr[i]           = gaussout.pcov[2,2]
+			stddev[i]           = np.sqrt(gaussout.pcov[2,2])
+		except:
+			print 'Error with gaussout.pcovp[2,2].'
+			print 'Value of gaussout is {}.'.format(gaussout)
 
 	davg=eavg/20.35 - 1
-	# _pdb.set_trace()
+	# pdb.set_trace()
 
 	# ======================================
 	# Set up basic beamline
 	# ======================================
+	# ----------------
+	# Get energy
+	# ----------------
+	B_spect = f[f['data']['raw']['scalars']['LI20_LGPS_3330_BDES']['dat'][0,0]][0,0]
+	gamma     = (20.35/0.5109989)*1e3
+
 	emitx = 0.000100/gamma
 	betax = 0.5
 	alphax = -1 
@@ -144,13 +267,23 @@ def wrap_analyze(infile):
 		   alpha=0
 		   )
 	beamline=bt.beamlines.IP_to_lanex(twiss_x=twiss,twiss_y=twiss,gamma=gamma)
+
+
+	print step_vals[0]
+
+	bact = E200.setQS.set_QS_energy_ELANEX(step_vals[0])
+	qs1_k_half = E200.setQS.bdes2K1(bact[0],20.35)
+	qs2_k_half = E200.setQS.bdes2K1(bact[1],20.35)
+
 	beamline.elements[1].K1 = qs1_k_half
 	beamline.elements[2].K1 = qs1_k_half
 	beamline.elements[4].K1 = qs2_k_half
 	beamline.elements[5].K1 = qs2_k_half
+
+	# pdb.set_trace()
 	
 	# ======================================
-	# Create beamline array??
+	# Create beamline array
 	# ======================================
 	gamma          = (1+davg)*39824
 	beamline_array = np.array([])
@@ -171,7 +304,9 @@ def wrap_analyze(infile):
 			variance,
 			emitx,
 			error=used_error,
-			verbose=True)
+			verbose=True,
+			plot=False
+			)
 	
 	# ======================================
 	# Plot results
@@ -180,10 +315,16 @@ def wrap_analyze(infile):
 			variance,
 			out.beta,
 			out.X_unweighted,
-			top='Data is real!',
+			top='Shot',
 			figlabel='Comparison',
-			error=used_error)
+			figpath='figs',
+			error=used_error,
+			axes=axplotfit
+			)
 
+	# pdb.set_trace()
+
+	return out
 
 	# figchisquare = plt.figure()
 	# plt.plot(davg,chisq_red)
@@ -194,7 +335,7 @@ def wrap_analyze(infile):
 	# print davg
 	# print chisq_red
 
-	# _pdb.set_trace()
+	# pdb.set_trace()
 
 if __name__ == '__main__':
         parser=argparse.ArgumentParser(description=
