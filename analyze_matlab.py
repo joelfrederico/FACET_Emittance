8b68c51b2af5bd5ee36b6619dd0b54120647f15e
Joel Frederico <joelfred@slac.stanford.edu>
2014-04-23 22:10:02 -0700

    Progress! Fits to real beam in python, no matlab.

--- analyze_matlab.py
+++ analyze_matlab.py
@@ -8,158 +8,135 @@ import mytools.slactrac as sltr
 import matplotlib.pyplot as plt
 import mytools as mt
 import copy
+import h5py as h5
 
-def analyze(infile):
-	# Load and transfer matlab variables
-	matvars        = sio.loadmat(infile)
-	processed_data = matvars['processed_data']
-	img_sub        = matvars['img_sub']
-	hist_data      = matvars['hist_data']
-	B5D36_en       = matvars['B5D36']
+plt.close()
 
-	sum_x     = processed_data['sum_x'][0,0]
-	sum_y     = processed_data['sum_y'][0,0]
-	x_meter   = processed_data['x_meter'][0,0]
-	B5D36_en  = B5D36_en.item(0)
-	new_en    = (B5D36_en-4.3)
-	gamma     = (B5D36_en/0.5109989)*1e3
-	new_gamma = (new_en/0.5109989)*1e3
+# ======================================
+# Load and transfer matlab variables
+# ======================================
+infile     = 'forpython.mat'
+f          = h5.File(infile);
+data       = f['data']
+imgstr       = data['raw']['images']['CMOS_FAR']
+res = imgstr['RESOLUTION'][0,0]
 
-	# Translate into script params
-	davg         = (hist_data[:,0]/16.05-1)
-	variance_old = hist_data[:,1]
-	filt         = np.logical_not( (davg>(0.9971-1)) & (davg < (1.003-1)))
-	filt         = np.logical_or(filt, np.logical_not(filt))
+# ======================================
+# Bend magnet strength
+# ======================================
+B5D36 = data['raw']['scalars']['LI20_LGPS_3330_BDES']['dat']
+B5D36 = mt.derefdataset(B5D36,f)
+B5D36_en  = B5D36[0]
+new_en    = (B5D36_en-4.3)
+gamma     = (B5D36_en/0.5109989)*1e3
+new_gamma = (new_en/0.5109989)*1e3
 
-	# variance_old = variance
-	
-	num_pts = len(sum_x)
-	variance  = np.zeros(num_pts)
-	stddev    = np.zeros(num_pts)
-	varerr    = np.zeros(num_pts)
-	chisq_red = np.zeros(num_pts)
-	for i,el in enumerate(sum_x):
-		y                   = sum_x[i,:]
-		popt,pcov,chisq_red[i] = mt.gaussfit(x_meter,y,sigma_y=np.sqrt(y),plot=False,variance_bool=True,verbose=False)
-		variance[i]         = popt[2]
-		varerr[i]           = pcov[2,2]
-		stddev[i]           = np.sqrt(pcov[2,2])
+# ======================================
+# Translate into script params
+# ======================================
+# xstart = 350
+# xstop  = 550
+# ystart = 225
+# ystop  = 280
+n_rows    = 3
+xstart = 275
+xstop  = 325
+ystart = 660
+ystop  = 690
+y_int = np.round((ystop-ystart)/n_rows)
+y_vec = mt.linspacestep(ystart,ystop,y_int)
+x_int = np.round((xstop-xstart)/n_rows)
+x_vec = mt.linspacestep(xstart,xstop,x_int)
+x_mean = np.mean(x_vec)
+x_axis = (x_vec-x_mean)*res*10**(-3)
 
-	# Set up initial conditions
-	# emitx = 100.1033e-6/40000 
-	# betax = 59.69009e-3
-	# alphax = -0.7705554
+# ======================================
+# Get image
+# ======================================
+# imgdat=mt.E200.E200_api_getdat(imgstr,f)
+img=mt.E200.E200_load_images(imgstr,f)
+img=img[8,xstart:xstop,ystart:ystop]
+# plt.imshow(np.rot90(img,k=-1))
+# plt.show()
 
-	# RMS
-	# emitx = 0.00201/gamma
-	# betax = 11.2988573693
-	# alphax = 6.72697997971
+# ======================================
+# Create a histogram of std dev
+# ======================================
+hist_vec  = mt.linspacestep(ystart,ystop,n_rows)
+n_groups  = np.size(hist_vec)
+# hist_data = np.zeros([n_groups,2])
+x_pix     = np.round(mt.linspacestep(xstart,xstop-1,1))
+x_meter   = (x_pix-np.mean(x_pix)) * res * 10**-6
+x_sq      = x_meter**2
 
-	# Gauss fit
-	emitx = 0.001363/gamma
-	betax = 8.7
-	alphax = 5.224
-	gammax = (1+np.power(alphax,2))/betax
-	twiss=np.array([betax,alphax,gammax])
-	T = np.array([[betax,-alphax],[-alphax,gammax]])
+num_pts = 11
+variance  = np.zeros(num_pts)
+stddev    = np.zeros(num_pts)
+varerr    = np.zeros(num_pts)
+chisq_red = np.zeros(num_pts)
+for i in mt.linspacestep(1,n_groups-1):
+	print i
+	sum_x = np.sum(img[:,(i-1)*n_rows:i*n_rows],1)
+	# plt.plot(x_meter,sum_x)
+	# plt.show()
+	popt,pcov,chisq_red[i] = mt.gaussfit(x_meter,sum_x,sigma_y=np.sqrt(sum_x),plot=False,variance_bool=True,verbose=True,background_bool=True)
+	variance[i]         = popt[2]
+	varerr[i]           = pcov[2,2]
+	stddev[i]           = np.sqrt(pcov[2,2])
 
-	# Create Beamline {{{
-	LIPOTR2TOR = sltr.Drift(length = 4.9266E+00)
-	LTOR2QS1 = sltr.Drift(length = 0.5E+00)
-	# QS1     : QUAD,L= 5.000000000E-01,K1= 3.077225846087095e-01,ORDER=2, DY=5.0e-30, DX=1.0e-30
-	QS1 = sltr.Quad(length= 5.000000000E-01,K1= 3.077225846087095e-01)
-	QS1._change_E(gamma,new_gamma)
-	LQS12QS2 = sltr.Drift(length = 4.00E+00)
-	# QS2     : QUAD,L= 5.000000000E-01,K1=-2.337527121004531e-01, ORDER=2
-	QS2 = sltr.Quad(length= 5.000000000E-01,K1=-2.337527121004531e-01)
-	QS2._change_E(gamma,new_gamma)
-	LQS22BEND = sltr.Drift(length = 0.7428E+00)
-	# B5D36_1 : CSBEN,L= 4.889500000E-01,          &
-	#                ANGLE= 3.0E-03, 	     &
-	#                EDGE1_EFFECTS=1,E1= 3.0E-3, &
-	#                EDGE2_EFFECTS=0,    	     &
-	#                HGAP= 3E-02, 		     &
-	#                TILT= 1.570796327E+00, &
-	#                SYNCH_RAD = 0
-	#                
-	# B5D36_2 : CSBEN,L= 4.88950000E-01,          &
-	#                ANGLE= 3.0E-03, 	     &
-	#                EDGE1_EFFECTS=0, 	     &
-	#                EDGE2_EFFECTS=1,E2= 3.0E-3, &
-	#                HGAP= 3E-02, 	    	     &
-	#                TILT= 1.570796327E+00, &
-	#                SYNCH_RAD = 0
-	B5D36 = sltr.Bend(
-			length= 2*4.889500000E-01,          
-	               angle= 6.0E-03, 	     
-		       order=1,
-		       rotate=0
-		       )
-	               
-	LBEND2TABLEv2 = sltr.Drift(length = 8.855E+00)
-	LTABLE2WAFERv2 = sltr.Drift(length = 1.045E+00)
-	
-	beamline = sltr.Beamline(
-			element_list=[
-				LIPOTR2TOR    ,
-				LTOR2QS1      ,
-				QS1           ,
-				QS1           ,
-				LQS12QS2      ,
-				QS2           ,
-				QS2           ,
-				LQS22BEND     ,
-				B5D36       ,
-				LBEND2TABLEv2 ,
-				LTABLE2WAFERv2
-				],
-			gamma= gamma
-			)
 
-	beamline.calc_mat()
-	beamline.change_energy(new_gamma);
-	# for el in beamline.elements:
-	#         print '---------------'
-	#         print 'Energy of {} = {}'.format(el._type,el._gamma)
-	#         try:
-	#                 print 'K1 = {}'.format(el._K1)
-	#         except:
-	#                 pass
-	#         try:
-	#                 print 'K2 = {}'.format(el._K2)
-	#         except:
-	#                 pass
-	#         try:
-	#                 print 'Length = {}'.format(el._length)
-	#         except:
-	#                 pass
-	# print '---------------'
-	#}}}
+# davg         = (hist_data[:,0]-1)
+# variance_old = hist_data[:,1]
+# filt         = np.logical_not( (davg>(0.9971-1)) & (davg < (1.003-1)))
+# filt         = np.logical_or(filt, np.logical_not(filt))
 
-	# Fit bowtie plot
-	chisq_factor = 1
-	# chisq_factor = 63.6632188
-	used_error   = stddev*np.sqrt(chisq_factor)
+# Set up initial conditions
+# emitx = 100.1033e-6/40000 
+# betax = 59.69009e-3
+# alphax = -0.7705554
 
-	out          = bt.fitbowtie(beamline,davg,variance,filt,T,twiss,emitx,error=used_error, verbose=True)
-	spotexpected = out.spotexpected
-	X            = out.X
-	beta         = out.beta
-	covar        = out.covar
-	# print covar
+# RMS
+# emitx = 0.00201/gamma
+# betax = 11.2988573693
+# alphax = 6.72697997971
 
-	figcher=plt.figure()
-	top='Simulated Energy Emittance Measurement\nNOT PHYSICAL'
-	bt.plotfit(filt,davg,variance,beta,out.X_unweighted,spotexpected,top,error=used_error)
+# Gauss fit
+emitx = 0.001363/gamma
+betax = 1
+alphax = 0
+gammax = (1+np.power(alphax,2))/betax
+twiss    = sltr.Twiss(
+		beta  = 0.5,
+		alpha = 0
+		)
 
-	figchisquare = plt.figure()
-	# plt.plot(davg,chisq_red)
-	mt.plot_featured(davg,chisq_red,'.-',
-			toplabel='Chi-Squared for Each Gaussian Fit',
-			xlabel='$E/E_0$',
-			ylabel='$\chi^2$')
-	# print davg
-	# print chisq_red
+# Create beamlines
+beamline=bt.beamlines.IP_to_lanex(twiss_x=twiss,twiss_y=twiss,gamma=gamma)
+
+# Fit bowtie plot
+chisq_factor = 1
+# chisq_factor = 63.6632188
+used_error   = stddev*np.sqrt(chisq_factor)
+
+out          = bt.fitbowtie(beamline,davg,variance,filt,T,twiss,emitx,error=used_error, verbose=True)
+spotexpected = out.spotexpected
+X            = out.X
+beta         = out.beta
+covar        = out.covar
+# print covar
+
+figcher=plt.figure()
+top='Simulated Energy Emittance Measurement\nNOT PHYSICAL'
+bt.plotfit(filt,davg,variance,beta,out.X_unweighted,spotexpected,top,error=used_error)
+
+figchisquare = plt.figure()
+# plt.plot(davg,chisq_red)
+mt.plot_featured(davg,chisq_red,'.-',
+		toplabel='Chi-Squared for Each Gaussian Fit',
+		xlabel='$E/E_0$',
+		ylabel='$\chi^2$')
+# print davg
+# print chisq_red
 
 if __name__ == '__main__':
 
--- matlab_script.m
+++ matlab_script.m
@@ -1,137 +1,25 @@
 function out = matlab_script(c,x_min,x_max,y_min,y_max)
 	global data
+	camname = 'ELANEX';
 	
 	% Usual data
-	day = '20130428';
-	data_set = 'E200_10849';
+	day = '20140422';
+	data_set = 'E200_12561';
 
-	% Hunt for clean, no-oven data
-	% day = '20130427'
-	% data_set = 'E200_10755';
 	if isempty(data)
 	
 		% day = '20130429';
 		% data_set = 'E200_10911';
-		pathstr=['/nas/nas-li20-pm01/E200/2013/' day '/' data_set '/']
+		pathstr=['/nas/nas-li20-pm00/E200/2014/' day '/' data_set '/' data_set '.mat']
 		
 		data=E200_load_data(pathstr);
 	end
 
-	% Wanted UIDs
-	% bool    = (data.raw.scalars.step_num.dat==3);
-	% imguids = intersect(imgstruct.UID,data.raw.scalars.step_num.UID(bool));
-	wanted_UIDs=data.raw.images.CELOSS.UID(c);
-
-	% Get step_value
-	% step_struct = data.raw.scalars.step_value;
-	% [this,bool] = ismember(wanted_UIDs,step_struct.UID);
-	% delE        = step_struct.dat(bool);
-	delE =0;
-	% display(delE)
-	
-	% Load the desired image
-	imgstruct=data.raw.images.CELOSS;
-	[imgs,bg]=E200_load_images(imgstruct,wanted_UIDs,data);
-	[this,bool] = ismember(wanted_UIDs,imgstruct.UID);
-	res         = imgstruct.RESOLUTION(bool);
-	
-	% Manipulate image to plot correctly
-	img_sub=imgs{1}-uint16(bg{1});
-	% img_sub=imgs{1};
-	img=img_sub;
-	% img=rot90(img);
-	img=transpose(img);
-	img=fliplr(img);
-	plotimg=log10(double(img));
-	plotimg=img;
-	% size(img)
-	
-	% Determine spectrometer bend settings/calibration
-	bend_struct = data.raw.scalars.LI20_LGPS_3330_BDES;
-	bool        = ismember(wanted_UIDs,bend_struct.UID);
-	% display(bend_struct.UID)
-	B5D36 = bend_struct.dat{1};
-	% display(B5D36);
-	e_axis=E200_cher_get_E_axis('20130423','CELOSS',0,[1:1392],0,B5D36);
-
-	% Plot Image
-	close('all');
-	cmap  = custom_cmap();
-	colormap(cmap.wbgyr);
-	% colormap(gray)
-	immax = max(max(plotimg));
-	% size(plotimg)
-	fig = imagesc(plotimg,[0,immax]);
-	out = fig;
-end
-
-function temp()
-
-	fig    = imagesc(plotimg(y_min:y_max,x_min:x_max),[0,immax]);
-	% fig   = surf(double(plotimg(y_min:y_max,x_min:x_max)));
-	y_int  = round((y_max-y_min)/10);
-	y_vec  = [y_min:y_int:y_max];
-	x_int  = round((x_max-x_min)/10);
-	x_vec  = [x_min:x_int:x_max];
-	x_mean = mean(x_vec);
-	x_axis = (x_vec-x_mean)*res*10^-3;
-
-	% set(gca,'YTick',y_vec-y_min,'YTickLabel',e_axis(y_vec),'YDir','normal', ...
-	%         'XTick',x_vec-x_min,'XTickLabel', x_axis...
-	%         );
-
-	% pcolor([1:size(img,2)],e_axis,img);
-	% shading('flat');
-
-	% Create a histogram of std dev
-	n_rows    = 10;
-	hist_vec  = [y_min:n_rows:y_max];
-	n_groups  = length(hist_vec);
-	hist_data = zeros(n_groups,2);
-	% length(hist_vec)
-	x_pix     = [x_min:x_max];
-	x_meter   = (x_pix-mean(x_pix)) * res * 10^-6;
-	x_sq      = x_meter.^2;
-	% size(x_sq)
-
-	processed_data.x_meter  = x_meter;
-	processed_data.e_axis   = e_axis;
-	processed_data.img      = img;
-	processed_data.x_min    = x_min;
-	processed_data.x_max    = x_max;
-	processed_data.n_rows   = n_rows;
-	processed_data.n_groups = n_groups;
-	processed_data.sum_x    = zeros(n_groups,x_max-x_min+1);
-	processed_data.sum_y    = zeros(n_groups,11);
-	
-	size(processed_data.sum_x)
-	for i=1:n_groups
-		y_pix_vec = [hist_vec(i):hist_vec(i)+n_rows];
-		subimg = transpose(img(y_pix_vec,x_min:x_max));
-		subimg_x = sum(subimg,2);
-		subimg_y = sum(subimg,1);
-
-		hist_data(i,2) = x_sq*subimg_x/sum(subimg_x);
-		% size(subimg_x)
-		% size(processed_data.sum_x(i,:))
-		% size(subimg_y)
-		processed_data.sum_x(i,:) = transpose(subimg_x);
-		processed_data.sum_y(i,:) = subimg_y;
-
-
-		% size(subimg_y)
-		% size(e_axis(y_pix_vec))
-		hist_data(i,1) = subimg_y*transpose(e_axis(y_pix_vec))/sum(subimg_y);
-	end
-	
-	% figure;
-	% plot(hist_data(:,1),hist_data(:,2)*(10^3)^2,'-o');
-	% tilefigs;
 	
 	% Save for python loading
-	save('forpython.mat','img','img_sub','hist_data','B5D36','processed_data','-v7');
+	save('forpython.mat','data','-v7.3');
 
-	setenv('DYLD_LIBRARY_PATH', '/usr/local/bin:/opt/local/lib:');
+	% setenv('DYLD_LIBRARY_PATH', '/usr/local/bin:/opt/local/lib:');
 	% unix('source ~/.profile; ./analyze_matlab.py forpython.mat -v');
 	out=data;
 end
