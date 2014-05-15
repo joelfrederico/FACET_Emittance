12cf0b88f4289f98f8616e75b5e5295461ef027b
Joel Frederico <joelfred@slac.stanford.edu>
2014-05-15 15:38:39 -0700

    Added rectangle to Mpl_Image

--- ButterflyGUI/ButterflyGUI.py
+++ ButterflyGUI/ButterflyGUI.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
+import mytools as mt
 import sys
 from PyQt4 import QtGui,QtCore
 import mainwindow_auto as mw
 import numpy as np
 
-class ButterflyGUI(mw.Ui_MainWindow):
-    def __init__(self, parent=None, name=None, fl=0):
-        mw.__init__(self,parent,name,fl)
+# class ButterflyGUI(mw.Ui_MainWindow):
+#     def __init__(self, parent=None, name=None, fl=0):
+#         mw.__init__(self,parent,name,fl)
 
-app = QtGui.QApplication(sys.argv)
+# app = QtGui.QApplication(sys.argv)
+app = mt.qt.get_app()
 window = QtGui.QMainWindow()
 ui = mw.Ui_MainWindow()
 ui.setupUi(window)
-x=np.linspace(-3,3,100)
-y=np.cos(x)
-mw.MplWidget.MplCanvas.plot(x,y)
+# x=np.linspace(-3,3,100)
+# y=np.cos(x)
+# ui.imageview_mpl.ax.plot(x,y)
 window.show()
-# sys.exit(app.exec_())
+app.exec_()
--- ButterflyGUI/mplwidget.py
+++ ButterflyGUI/mplwidget.py
@@ -1,20 +1,64 @@
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 from PyQt4 import QtGui
 import matplotlib as mpl
 # import matplotlib.figure as plt
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+import numpy as np
 
-class MplCanvas(FigureCanvas):
-	def __init__(self):
+# class MplCanvas(FigureCanvas):
+# 	def __init__(self):
+# 		self.fig=mpl.figure.Figure()
+# 
+# 		self.ax=self.fig.add_subplot(111)
+# 		self.rect = mpl.patches.Rectangle((0,0),1,1)
+# 		FigureCanvas.__init__(self,self.fig)
+# 		FigureCanvas.setSizePolicy(self,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding)
+# 		FigureCanvas.updateGeometry(self)
+
+	#         self.fig.canvas.mpl_connect('figure_enter_event', self.enter_figure)
+
+	# def enter_figure(self,event):
+	#         print 'hello'
+
+# class MplWidget(QtGui.QWidget):
+# 	def __init__(self, parent=None):
+# 		QtGui.QWidget.__init__(self,parent)
+# 		self.canvas=MplCanvas()
+# 		self.vbl=QtGui.QVBoxLayout()
+# 		self.vbl.addWidget(self.canvas)
+# 		self.setLayout(self.vbl)
+
+class Mpl_Image(FigureCanvas):
+	def __init__(self, parent=None):
 		self.fig=mpl.figure.Figure()
-		self.ax=self.fig.add_subplot(111)
 		FigureCanvas.__init__(self,self.fig)
 		FigureCanvas.setSizePolicy(self,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding)
 		FigureCanvas.updateGeometry(self)
+		self.fig.canvas.mpl_connect('button_press_event', self.on_press)
+		self.fig.canvas.mpl_connect('button_release_event', self.on_release)
 
-class MplWidget(QtGui.QWidget):
-	def __init__(self, parent=None):
-		QtGui.QWidget.__init__(self,parent)
-		self.canvas=MplCanvas()
-		self.vbl=QtGui.QVBoxLayout()
-		self.vbl.addWidget(self.canvas)
-		self.setLayout(self.vbl)
+		self.ax=self.fig.add_subplot(111)
+
+		img = np.random.randn(10,10)
+		self.rect = mpl.patches.Rectangle((-10,0),0,3,facecolor='w',edgecolor='r',alpha=0.5)
+
+		self.ax.imshow(img)
+		self.ax.add_patch(self.rect)
+
+	def on_press(self,event):
+		print 'press'
+		self.x0 = event.xdata
+		self.y0 = event.ydata
+
+	def on_release(self,event):
+		print 'release'
+		self.x1 = event.xdata
+		self.y1 = event.ydata
+		self.rect.set_width(self.x1 - self.x0)
+		self.rect.set_height(self.y1 - self.y0)
+		self.rect.set_xy((self.x0, self.y0))
+		self.ax.figure.canvas.draw()
+
+class Mpl_Plot(Mpl_Image):
+	pass
