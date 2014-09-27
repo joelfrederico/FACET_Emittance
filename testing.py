63f900883c9dc9d3aba0786f938f09e8ba8c3084
Joel Frederico <joelfred@slac.stanford.edu>
2014-09-26 19:18:08 -0700

    Updated testing

--- testing.py
+++ testing.py
@@ -13,10 +13,5 @@ import h5py as h5
 # print buttonbox.clickedArray()
 
 # data = mt.E200.E200_load_data('/nas/nas-li20-pm00/E200/2014/20140629/E200_13537/E200_13537.mat')
-data = mt.E200.Filename('nas/nas-li20-pm00/E200/2014/20140629/E200_13537/E200_13537.mat')
+data = mt.E200.Data('nas/nas-li20-pm00/E200/2014/20140629/E200_13537/E200_13537.mat')
 
-
-print data.dir_beg
-print data.dir_mid
-print data.filename
-print data.data_source_type
