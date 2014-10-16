e58058f6bb7b35df4b8852aa717f0610f3444c68
Joel Frederico <joelfred@slac.stanford.edu>
2014-10-16 16:01:24 -0700

    Updated testing

--- testing.py
+++ testing.py
@@ -5,46 +5,141 @@ import mytools.qt as myqt
 from PyQt4 import QtGui,QtCore
 import h5py as h5
 import numpy as np
-
-# app = myqt.get_app()
-# buttons=[myqt.Button(QtGui.QMessageBox.Ok),myqt.Button('this',QtGui.QMessageBox.HelpRole,buttontype='Default')]
-# # buttons=None
-# buttonbox=myqt.ButtonMsg('this','that',buttons=buttons)
-# 
-# print buttonbox.clickedArray()
+import ButterflyEmittancePython as bt
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 try:
-	wf.close()
+	data.close()
 except:
 	pass
 
-data = mt.E200.E200_load_data('nas/nas-li20-pm00/E200/2014/20140625/E200_13438')
-# data = mt.E200.Data('nas/nas-li20-pm00/E200/2014/20140629/E200_13537/E200_13537.mat')
-# data = mt.E200.Filename('nas/nas-li20-pm00/E200/2014/20140629/E200_13537/E200_13537.mat')
+sets      = ['20140625','13438']
+setdate   = sets[0]
+setnumber = sets[1]
 
-wf = data.write_file
-vectors=wf['data']['processed']['vectors']
+loadfile = 'nas/nas-li20-pm00/E200/2014/{}/E200_{}'.format(setdate,setnumber)
+data = mt.E200.E200_load_data(loadfile)
 
-# val = [50,50]
-# uid=1
-# mt.E200.E200_api_updateUID(vectors,val,uid)
+wf        = data.write_file
+processed = wf['data']['processed']
+scalars   = processed['scalars']
+vectors   = processed['vectors']
+arrays    = processed['arrays']
 
-# print '==========================='
+# ======================================
+# Scalar test
+# ======================================
+# Set up new things for scalar
+verbose = True
+if verbose:
+	print r'''
+=================================
+Scalar test
+================================='''
+test_scalar = mt.E200.E200_create_data(scalars,'test_scalar',datatype='array')
+test_uids   = np.array([6,7,8,1,4,5])
+test_values = np.array(test_uids*1.1,dtype=np.float64)
 
-# newval = [10,20]
-# newuid = 2
+# Add scalars
+mt.E200.E200_api_updateUID(test_scalar,test_uids,test_values,verbose=verbose)
 
-# mt.E200.E200_api_updateUID(vectors,newval,newuid)
+# Show scalars
+if verbose:
+	print test_scalar['UID'].value
+	for val in test_scalar['dat'].value:
+		print test_scalar.file[val].value
 
-# print '==========================='
+# Change scalars
+test_uids   = np.array([9,6])
+test_values = np.array(test_uids*-1.1,dtype=np.float64)
 
-# newval = [1,2]
-# newuid = 3
+# Update scalars
+mt.E200.E200_api_updateUID(test_scalar,test_uids,test_values,verbose=verbose)
 
-# mt.E200.E200_api_updateUID(vectors,newval,newuid)
+# Show scalars
+if verbose:
+	print test_scalar['UID'].value
+	for val in test_scalar['dat'].value:
+		print test_scalar.file[val].value
 
-# print '==========================='
+# ======================================
+# Vector test
+# ======================================
+# Set up new things for vector
+verbose = True
+if verbose:
+	print r'''
+=================================
+Vector test
+================================='''
+test_vector = mt.E200.E200_create_data(vectors,'test_vector',datatype='array')
+test_uids   = np.array([6,7,8,1,4,5])
+base = np.array([1,10,100],dtype=np.float64)
+test_values = np.array(np.outer(test_uids,base),dtype=np.float32)
 
-# out = mt.E200.E200_api_getdat(vectors,uids=[3,1])
-# print out.dat
-# print out.uid
+# Add vectors
+mt.E200.E200_api_updateUID(test_vector,test_uids,test_values,verbose=verbose)
+
+# Show vectors
+if verbose:
+	print test_vector['UID'].value
+	for val in test_vector['dat'].value:
+		print test_vector.file[val].value
+
+# Change vectors
+test_uids   = np.array([9,6])
+base = np.array([-1,-10,-100],dtype=np.float64)
+test_values = np.array(np.outer(test_uids,base),dtype=np.float32)
+
+# Update vectors
+mt.E200.E200_api_updateUID(test_vector,test_uids,test_values,verbose=verbose)
+
+# Show vectors
+if verbose:
+	print test_vector['UID'].value
+	for val in test_vector['dat'].value:
+		print test_vector.file[val].value
+
+
+# ======================================
+# Array test
+# ======================================
+# Set up new things for array
+verbose = True
+if verbose:
+	print r'''
+=================================
+Array test
+================================='''
+test_array = mt.E200.E200_create_data(arrays,'test_array',datatype='array')
+test_uids   = np.array([6,7,8,1,4,5])
+base_array = np.array([[1,10],[100,1000]])
+test_values = np.empty(test_uids.shape,dtype=np.object)
+for i,val in enumerate(test_uids):
+	test_values[i] = base_array*val
+
+# Add arrays
+mt.E200.E200_api_updateUID(test_array,test_uids,test_values,verbose=verbose)
+
+# Show arrays
+if verbose:
+	print test_array['UID'].value
+	for val in test_array['dat'].value:
+		print test_array.file[val].value
+
+# Change arrays
+test_uids   = np.array([9,6])
+base_array = np.array([[1,10,100],[-100,-10,-1]])
+test_values = np.empty(test_uids.shape,dtype=np.object)
+for i,val in enumerate(test_uids):
+	test_values[i] = base_array*val
+
+# Update arrays
+mt.E200.E200_api_updateUID(test_array,test_uids,test_values,verbose=verbose)
+
+# Show arrays
+if verbose:
+	print test_array['UID'].value
+	for val in test_array['dat'].value:
+		print test_array.file[val].value
